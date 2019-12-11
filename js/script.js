82139893afe2a7e175c3cebe4ea6afad7a64fd5b
Alexey <droterlol@mail.ru>
2019-12-11 17:37:17 +0500

    continued development of the project

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 let money,
     time;
 
-money = prompt("Ваш бюджет на месяц?");
+money = +prompt("Ваш бюджет на месяц?");
 time = prompt("Введите дату в формате YYYY-MM-DD");
 
 let appData = {
@@ -15,20 +15,63 @@ let appData = {
     savings: false
 };
 
-let question1 = prompt("Введите обязательную статью расходов в этом месяце"),
-    question2 = prompt("Во сколько обойдется?"),
-    question3 = prompt("Введите обязательную статью расходов в этом месяце"),
-    question4 = prompt("Во сколько обойдется?");
+for (let i = 0; i < 2; i++) {
 
-appData.expenses.question1 = question2;
-appData.expenses.question3 = question4;
+    let q = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        b = prompt("Во сколько обойдется?", "");
 
-alert(appData.budget/30);
+    if (q != "" && b != "" && q.length < 30 && typeof (q) === "string" &&
+     typeof (q) != null && typeof (b) != null) {
+        console.log("done");
+        appData.expenses[q] = b;
+    } else {
+        alert("Неверно введено");
+        i--;
+    }
 
+}
 
+// let i = 0;
+// while (i < 2) {
+//     let q = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//         b = prompt("Во сколько обойдется?", "");
 
+//     if (q != "" && b != "" && q.length < 30 &&
+//         typeof (q) === "string" && typeof (q) != null && typeof (b) != null) {
+//         console.log("done");
+//         appData.expenses[q] = b;
+//         i++;
+//     } else {
+//         alert("Неверно введено");
+//         continue;
+//     }
+// } 
 
+// let i = 0;
+// do {
+//     let q = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//         b = prompt("Во сколько обойдется?", "");
 
+//     if (q != "" && b != "" && q.length < 30 &&
+//         typeof (q) === "string" && typeof (q) != null && typeof (b) != null) {
+//         console.log("done");
+//         appData.expenses[q] = b;
+//         i++;
+//     } else {
+//         alert("Неверно введено");
+//     }
+// }
+// while (i < 2);
 
+appData.moneyPerDay = appData.budget / 30;
 
+if (appData.moneyPerDay <= 500) {
 
+    alert("Низкий уровень дохода, твой расход в день: " + appData.moneyPerDay);
+} else if (appData.moneyPerDay <= 2000 && appData.moneyPerDay > 500) {
+    alert("Средний уровень дохода, твой расход в день: " + appData.moneyPerDay);
+} else if (appData.moneyPerDay > 2000) {
+    alert("Высокий уровень дохода, твой расход в день: "+ appData.moneyPerDay);
+} else {
+    alert("С твоим бюджетом что-то не так...");
+}
